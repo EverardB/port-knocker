c194d055cc936f10b6cdd5db5ce1831ba2921947
Everard Brown <everard.brown@htcl.eu>
2012-10-25 22:56:02 +0000

    A more reliable knock algorithm
    git-svn-id: https://dev.htcl.eu/project/repo/port_knocker/PortKnocker-android@3 3d0b249a-b3b8-4e90-90b3-1f63537304ea

--- port-knocker/src/main/java/eu/htcl/android/portknocker/Knocker.java
+++ port-knocker/src/main/java/eu/htcl/android/portknocker/Knocker.java
@@ -6,12 +6,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import java.io.IOException;
-import java.net.ConnectException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 
 /**
  *
@@ -20,20 +15,24 @@ import java.net.UnknownHostException;
 public class Knocker {
 
     public static boolean doKnock(String host, int port, String protocol) {
+        Socket tcp_socket = null;
+        DatagramSocket udp_socket = null;
+        InetAddress inetAddress = null;
 
         try {
             if ("udp".equals(protocol)) {
                 // udp knock (a ping would wait for a connection/resonse)
-                DatagramSocket udp_socket = new DatagramSocket();
-                InetAddress inetAddress = InetAddress.getByName(host);
+                udp_socket = new DatagramSocket();
+                inetAddress = InetAddress.getByName(host);
                 String message = "-";
                 DatagramPacket request = new DatagramPacket(message.getBytes(), message.length(), inetAddress, port);
                 udp_socket.send(request);
-                udp_socket.close();
             } else {
                 // tcp knock (a ping would wait for a connection/resonse)
-                Socket tcp_socket = new Socket(host, port);
-                tcp_socket.close();
+                tcp_socket = new Socket();
+                inetAddress = InetAddress.getByName(host);
+                SocketAddress socketAddress = new InetSocketAddress(inetAddress, port);
+                tcp_socket.connect(socketAddress, 100);
                 //if (tcp_socket.isConnected()) {
                 //    tcp_socket.close();
                 //}
@@ -46,6 +45,17 @@ public class Knocker {
         } catch (IOException e) {
             e.printStackTrace();
             return false;
+        } finally {
+            if (udp_socket != null) {
+                udp_socket.close();
+            }
+            if (tcp_socket != null) {
+                try {
+                    tcp_socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         return true;
